a0c3c38d90015c92cd5f1552dadb3f3003c15c8c
Harlan T Wood <code@harlantwood.net>
2021-06-08 14:00:01 -1000

    Build in CI; script cleanup

--- build_languages.sh
+++ build_languages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
 for lang in "languages" "agent-profiles" "shared-perspectives" "ipfs-links" "note-ipfs"
 do
-echo "============================"
-echo "BUILDING LANGUAGE" $lang
-cd src/languages/$lang
-npm i && npm run build && echo "SUCCESSFULLY BUILT" $lang || echo "ERROR BUILDING" $lang "!!!"
-cd -
-echo "============================"
+  echo "============================"
+  echo "BUILDING LANGUAGE" $lang
+  cd src/languages/$lang
+  npm i && npm run build && echo "SUCCESSFULLY BUILT" $lang || echo "ERROR BUILDING" $lang "!!!"
+  cd -
+  echo "============================"
 done
-exit 0
